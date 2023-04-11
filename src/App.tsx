6eed1563ab7ad6f14867abfe70e8f82392ebfb25
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2023-04-11 19:17:51 -0300

    Navbar/Footer/Home done

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/navbar/Navbar'
-import Footer from './components/footer/Footer'
+import Navbar from './components/estaticos/navbar/Navbar'
+import Footer from './components/estaticos/footer/Footer'
+import Home from './paginas/home/Home'
 
 function App() {
   return (
     <>
       <Navbar />
-      
+      <Home />
+      <Footer />
     </>
   )
 }
