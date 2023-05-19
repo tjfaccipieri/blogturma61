8c0c880dfcdd2a799bd304a311d12b629723b20d
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2023-05-19 08:48:06 -0300

    footer finalizado

--- src/components/postagens/tabPostagens/TabPostagens.tsx
+++ src/components/postagens/tabPostagens/TabPostagens.tsx
@@ -7,6 +7,7 @@ import TabPanel from '@material-ui/lab/TabPanel';
 import ListaPostagens from '../listaPostagens/ListaPostagens';
 import './TabPostagens.css'
 import Carousel from '../../estaticos/carousel/Carousel';
+import { Typography } from '@mui/material';
 
 function TabPostagens() {
   const [value, setValue] = React.useState('1');
@@ -27,7 +28,16 @@ function TabPostagens() {
           <ListaPostagens />
         </TabPanel>
         <TabPanel value="2">
-          <Carousel />
+          <Typography variant='body1'>
+          Hoje iremos apresentar uma receita fácil e prática para um lanche da tarde ou para reuniões com amigos e família.
+          </Typography>
+          <ul>
+            <li>1 pacote de miojo de qualquer sabor (você não vai usar o sachê)</li>
+            <li>4-5 dentes de alho</li>
+            <li>3 colheres de sopa de manteiga</li>
+            <li>sal e pimenta-do-reino a gosto</li>
+          </ul>
+          <Typography variant='body1'>Viu como é prática?, além de ter um fácil preparo ainda vai fazer muito sucesso com a criançada e com a galera reunida.</Typography>
         </TabPanel>
       </TabContext>
   )
